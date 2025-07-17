7a6cd38ab9a816b2eb1a09574153d50ee316a0c9
Bang04 <eunkyeong_b@naver.com>
2025-07-17 18:47:31 +0900

    style : 텍스트 , 이미지 추가

--- sunnyside-agency-landing-page/src/App.tsx
+++ sunnyside-agency-landing-page/src/App.tsx
@@ -9,23 +9,28 @@ function App() {
   return (
     <div className='flex flex-col'>
       <div className="bg-[url('/src/assets/images/mobile/image-header.jpg')] bg-cover  md:bg-[url('/src/assets/images/desktop/image-header.jpg')] bg-center bg-no-repeat">
-          <div className='flex flex-row '>
+          <div className='flex flex-row content-between'>
+
+
             <div className='text-blue-600'><img src='/src/assets/images/logo.svg' /></div>
             
-            <div className='md:hidden'><button><img src='/src/assets/images/icon-hamburger.svg' /></button></div>
-            <div className='flex flex-row hidden md:flex'>
-                <div>About</div> 
-                <div>Services</div> 
-                <div>Projects</div> 
-                <div>Contact</div> 
+            <div>
+              <div className='md:hidden'><button><img src='/src/assets/images/icon-hamburger.svg' /></button></div>
+              <div className='flex flex-row hidden md:flex'>
+                  <div>About</div> 
+                  <div>Services</div> 
+                  <div>Projects</div> 
+                  <div>Contact</div> 
+              </div>
             </div>
+            
           </div>
             
-          <div>We are creatives </div>
+          <div className='text-5xl font-bold text-white'>We are creatives </div>
           <img src='/src/assets/images/icon-arrow-down.svg' />
       </div>
 
-      <div>  
+      <div className='text-center'>  
             <div>
                 <div>
                   <div>Transform your brand</div>
@@ -44,9 +49,7 @@ function App() {
             </div>
       </div>
       
-      <div>   ./ㅋAsdfghjkl;'
-        ';lkjhgfcdsxzaZAsxdcfvgbhl.;/'
-        gfdq  wer45to0p-[=\=-098765431' 
+      <div className='text-center'>
           <div className="bg-[url('/src/assets/images/mobile/image-graphic-design.jpg')]">
               <div>Graphic design Great design </div>
               <div>Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential clients’ attention.</div>
@@ -56,45 +59,49 @@ function App() {
               <div>Increase your credibility by getting the most stunning, high-quality photos that improve your business image.</div>
           </div>
       </div>
-      <div>
+      <div className='text-center'>
           <div>Client testimonials</div>
           <div>
-              <div className="bg-[url('/src/assets/images/image-emily.jpg')]"></div>
+              <div className="m-auto rounded-full bg-[url('/src/assets/images/image-emily.jpg')] bg-no-repeat bg-center bg-cover w-20 h-20" ></div>
               <div>We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit. </div>
               <div>Emily R. </div>
               <div>Marketing Director </div>
           </div>
           <div>
-              <div className="bg-[url('/src/assets/images/image-thomas.jpg')]">frofile</div>
+              <div className="m-auto rounded-full bg-[url('/src/assets/images/image-thomas.jpg')] bg-no-repeat bg-center bg-cover w-20 h-20"></div>
               <div>Sunnyside’s enthusiasm coupled with their keen interest in our brand’s success made it a satisfying and enjoyable experience.</div>
               <div>Thomas S.</div>
               <div>Chief Operating Officer</div>
           </div>
           <div>
-              <div className="bg-[url('/src/assets/images/image-emily.jpg')]">frofile</div>
+              <div className="m-auto rounded-full bg-[url('/src/assets/images/image-jennie.jpg')] bg-no-repeat bg-center bg-cover w-20 h-20"></div>
               <div>Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!</div>
               <div>Jennie F.</div>
               <div>Business Owner</div>
           </div>
       </div>
-      <div>
-          <div>img1</div>
-          <div>img2</div>
-          <div>img3</div>
-          <div>img4</div>
+      <div className='flex flex-col'>
+          <div className='flex flex-row w-full h-40'>
+              <div className="w-1/2 h-50 bg-[url('/src/assets/images/mobile/image-gallery-milkbottles.jpg')] bg-cover"></div>
+              <div className="w-1/2 bg-[url('/src/assets/images/mobile/image-gallery-orange.jpg')] bg-cover"></div>
+          </div>
+          <div className='flex flex-row w-full h-40'>
+              <div className="w-1/2 bg-[url('/src/assets/images/mobile/image-gallery-cone.jpg')] bg-cover"></div>
+              <div className="w-1/2 bg-[url('/src/assets/images/mobile/image-gallery-sugar-cubes.jpg')] bg-cover"></div>
+          </div> 
       </div>
       <div>
           <div></div>
-          <div>
-              <div>About</div>
-              <div>Services</div>
-              <div>Projects</div>
+          <div className='flex flex-row'>
+              <div className='w-1/3'>About</div>
+              <div className='w-1/3'>Services</div>
+              <div className='w-1/3'>Projects</div>
           </div>
-          <div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
+          <div className='flex flex-row'>
+              <div><img src='/src/assets/images/icon-facebook.svg'/> </div>
+              <div><img src='/src/assets/images/icon-instagram.svg'/></div>
+              <div><img src='/src/assets/images/icon-twitter.svg'/></div>
+              <div><img src='/src/assets/images/icon-pinterest.svg'/></div>
           </div>
       </div>
     </div>
@@ -102,3 +109,5 @@ function App() {
 }
 
 export default App
+
+
