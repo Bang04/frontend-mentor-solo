896fdf1cebd5b7492f6ff437b01afc6852452b68
Bang04 <eunkyeong_b@naver.com>
2025-07-23 16:31:10 +0900

    style : hover 폰트 굵기 삭제

--- sunnyside-agency-landing-page/src/App.tsx
+++ sunnyside-agency-landing-page/src/App.tsx
@@ -149,10 +149,10 @@ function App() {
             </div>
             <div className='flex min-w-3xs  bg-white text-center '>
                 <div className='flex-col md:flex-row my-7 mx-2'>
-                    <div className='text-2xl text-gray-400 px-18 py-5 hover:font-bold  hover:text-gray-800 hover:bg-yellow-300 hover:rounded-full'>About</div>
-                    <div className='text-2xl text-gray-400 px-18 py-5 hover:font-bold  hover:text-gray-800 hover:bg-yellow-300 hover:rounded-full'>Services</div>
-                    <div className='text-2xl text-gray-400 px-18 py-5 hover:font-bold  hover:text-gray-800 hover:bg-yellow-300 hover:rounded-full'>Projects</div>
-                    <div className='text-2xl text-gray-400 px-15 py-3 hover:font-bold  hover:text-gray-800 hover:bg-yellow-300 hover:rounded-full'>CONTACT</div>
+                    <div className='text-2xl text-gray-400 px-18 py-5  hover:text-gray-800 hover:bg-yellow-300 hover:rounded-full'>About</div>
+                    <div className='text-2xl text-gray-400 px-18 py-5  hover:text-gray-800 hover:bg-yellow-300 hover:rounded-full'>Services</div>
+                    <div className='text-2xl text-gray-400 px-18 py-5  hover:text-gray-800 hover:bg-yellow-300 hover:rounded-full'>Projects</div>
+                    <div className='text-xl text-gray-400 px-15 py-3  hover:text-gray-800 hover:bg-yellow-300 hover:rounded-full'>CONTACT</div>
                 </div>
             </div>
         </div>
